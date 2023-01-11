1ef21491e596ee48dcba372ab6a9b122c0bdfdf4
nadkon <nadkon@gmail.com>
2023-01-11 21:43:13 +0200

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+ n = n.toString();
+  let newN = Math.abs(n).toString();
+  let number = '';
+      for (let i = newN.length - 1; i >= 0; i--) {
+        number += newN[i];
+      }
+  if (n[0] == '-') {
+    number = `+${number}`
+  } else if (n[0] == '+') {
+    number = `-${number}`
+  } else {
+    number = number;
+  }
+return (number)
+  }
